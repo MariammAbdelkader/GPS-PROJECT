a4d1b89fad4bd38177e65e5a68ff1457ad46ce15
Jasmine Mamdouh <112454290+Jasmine2M@users.noreply.github.com>
2023-05-14 00:27:09 +0300

    Update UART.h

--- UART.h
+++ UART.h
@@ -5,3 +5,11 @@ char UART0_RecieveChar (void);
 void UART0_TransmitChar (char data);
 void UART0_TransmitString(char *str);
 void UART0_RecieveString(char *str, char stopCh);
+
+//UART 5
+void UART5_Init(void);
+
+char UART5_RecieveChar (void);
+void UART5_TransmitChar (char data);
+void UART5_TransmitString(char *str);
+void UART5_ReceiveString(char *str, char stopCh);
