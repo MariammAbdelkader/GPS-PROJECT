cd5104c22d2d9916aa74cea080c7d106a4b61905
marina-ebraam <133531586+marina-ebraam@users.noreply.github.com>
2023-05-14 00:07:23 +0300

    Create GPS.h

---
+++ GPS.h
@@ -0,0 +1,8 @@
+double toDegree(double angle);
+
+double toRad(double angle);
+
+double GPS_getdistance(double longA, double lattA, double longB, double lattB);
+void GPS_Read(void);
+void check();
+//float GPS_F(void);
